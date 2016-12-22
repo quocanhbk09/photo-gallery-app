22aef53e3cea5bf352bd786288b134d3ed0d273a
Anh Tran <quocanh.bk09@gmail.com>
2016-12-22 21:55:19 +0700

    Initial Commit

---
+++ Photo Gallery App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photo Gallery App
+//
+//  Created by Quoc Anh Tran on 12/22/16.
+//  Copyright © 2016 Anh Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
